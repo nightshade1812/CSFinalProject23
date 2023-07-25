5adc5178ee94e99d6012aa5dbdeb258a3434b42d
nightshade1812 <126979626+nightshade1812@users.noreply.github.com>
2023-07-25 09:57:41 -0400

    updated return types

--- Final Project/Searcher.java
+++ Final Project/Searcher.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 public class Searcher
 {
-   public static String linear(String[] array, String target)
+   public static boolean linear(String[] array, String target)
    {
       for(int i = 0; i < array.length; i++) {
          if(target.equalsIgnoreCase(array[i]))
-            return array[i];
+            return true;
       }
-      return "not found >:(";
+      return false;
    }
    
-   public static String binary(String[] array, String target, int first, int last)
+   public static boolean binary(String[] array, String target, int first, int last)
    {
       int middle = (first + last)/2;
       
@@ -21,7 +21,7 @@ public class Searcher
       else if(target.compareTo(array[middle]) > 0)
          return binary(array, target, middle - 1, last);
       else if(target.equalsIgnoreCase(array[middle]))
-         return array[middle];
-      return "not found >:(";
+         return true;
+      return false;
    }
 }
