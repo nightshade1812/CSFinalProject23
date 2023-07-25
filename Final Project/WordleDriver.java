b2ea51229b272f7000bd0de6e4e42b5ebff6b871
nightshade1812 <126979626+nightshade1812@users.noreply.github.com>
2023-07-25 12:25:53 -0400

    added window listener

--- Final Project/WordleDriver.java
+++ Final Project/WordleDriver.java
@@ -1,14 +1,26 @@
 import javax.swing.*; //JPanel class
+import java.awt.event.*; //WindowListener class
 
 public class WordleDriver
 {
+   static Scoreboard scoreboard = new Scoreboard();
+   
    public static void main(String[] args)
    {
       JFrame frame = new JFrame("Wordle - Final Project (Sarah and Satik)");
       frame.setSize(400, 625);
       frame.setLocation(200, 100);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.addWindowListener(new SaveAndClose());
       frame.setContentPane(new WordlePanel());
       frame.setVisible(true);
+   }
+   
+   private static class SaveAndClose extends WindowAdapter 
+   {
+      public void windowClosing(WindowEvent windowEvent) 
+      {
+         scoreboard.saveData();
+         System.exit(0);
+      }
    }
 }
