d5973c823e261aef8fb623d5999135bb72562f17
nightshade1812 <126979626+nightshade1812@users.noreply.github.com>
2023-07-25 14:56:59 -0400

    Update WordleDriver.java

--- Final Project/WordleDriver.java
+++ Final Project/WordleDriver.java
@@ -3,12 +3,13 @@ import java.awt.event.*; //WindowListener class
 
 public class WordleDriver
 {
-   static Scoreboard scoreboard = new Scoreboard();
+   static Scoreboard scoreboard;
    
    public static void main(String[] args)
    {
+      scoreboard = new Scoreboard();
       JFrame frame = new JFrame("Wordle - Final Project (Sarah and Satik)");
-      frame.setSize(425, 625);
+      frame.setSize(550, 655);
       frame.setLocation(200, 100);
       frame.addWindowListener(new SaveAndClose());
       frame.setContentPane(new WordlePanel());
