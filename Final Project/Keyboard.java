a5b09863d4f079ef522dc5c205fbf24eb4d481dd
nightshade1812 <126979626+nightshade1812@users.noreply.github.com>
2023-07-25 13:44:21 -0400

    Update Keyboard.java

--- Final Project/Keyboard.java
+++ Final Project/Keyboard.java
@@ -1,44 +1,20 @@
 import javax.swing.*; //JPanel class and other graphics objects
 import java.awt.*; //Layouts and other graphics objects
-import java.awt.event.*; //Listener class
 
-/*******
-*The WordlePanel class serves as the organizing panel for the game, containing several subpanels and menus/player options. 
+/**
+*The Keyboard class contains a QWERTY keyboard display that changes depending on which letters the player has guessed.
 */
-
-public class WordlePanel extends JPanel
+public class Keyboard extends JPanel
 {
-   /**
-   *The WordlePanel' Gameboard
-   @see Gameboard
-   */
-   private Gameboard gameboard;
-   
-   /**
-   *The WordlePanel' Scoreboard
-   @see Scoreboard
-   */
-   private Scoreboard scoreboard;
-   
-   /**
-   *The WordlePanel' Keyboard
-   @see Keyboard
-   */
-   private Keyboard keyboard;
-   
-   /**
-   *A JButton that resets the WordlePanel and its subpanels
-   @see JButton
-   */
-   private static JButton reset;
-   
-   /**
-   *An int that determines the amount of guesses in the game
-   */   
-   //guessLimit starts at 6, as is traditional for Wordle
-   private int guessLimit = 6;
-   
-   /****************************
+
+/**
+*A JLabel array instance that stores individual letters
+@see JLabel
+@see java.util.Arrays
+*/
+   private static JLabel[] keyboard;
+  
+  /****************************
    *Fills the panel with white
    */
    public void paintComponent(Graphics g)
@@ -47,74 +23,101 @@ public class WordlePanel extends JPanel
       g.fillRect(0, 0, 600, 600);
    }
    
-   /****************************
-   *Creates a WordlePanel instance containing a Gameboard object, a Scoreboard object, a KeyBoard object, and a JButton object
-   */
-   public WordlePanel()
+/**
+*Creates a Keyboard instance with a JLabel array
+*/
+   public Keyboard()
    {
-      //initializing the objects we will need
-      setLayout(new FlowLayout());
-      
-      gameboard = new Gameboard();
-      scoreboard = new Scoreboard();
-      keyboard = new Keyboard();
-      reset = new JButton("Reset");
-      reset.setEnabled(false);
-      reset.addActionListener(new ResetListener());
-      
-      add(scoreboard);
-      add(gameboard);
-      add(keyboard);
-      add(reset);
-   }
+      setLayout(new GridLayout(3, 10, 1, 1));
    
-   /**
-   *Resets the Gameboard and the Keyboard by calling their reset methods
-   @see Gameboard.reset()
-   @see Keyboard.reset()
-   */
+      keyboard = new JLabel[26];
+      
+      assignKeys(keyboard);
+      for(int k = 0; k < 26; k++) {
+         keyboard[k].setFont(new Font("Arial", Font.BOLD, 35));
+         keyboard[k].setOpaque(true);
+         keyboard[k].setBackground(Color.WHITE);
+      }
+      
+      for(int k = 0; k < 10; k++)
+         add(keyboard[k]);
+      
+      JLabel shortBlank = new JLabel(" ");
+      add(shortBlank);
+      
+      for(int k = 10; k < 19; k++)
+         add(keyboard[k]);
+      
+      JLabel longBlank = new JLabel("  ");
+      add(longBlank);
+      
+      for(int k = 19; k < 26; k++)
+         add(keyboard[k]);
+   }
+
+/**
+*Adds JLabels to keyboard with letters in traditional QWERTY order
+*/
+
+   public static void assignKeys(JLabel[] arr)
+   {
+      arr[0] = new JLabel("Q", SwingConstants.CENTER);
+      arr[1] = new JLabel("W", SwingConstants.CENTER);
+      arr[2] = new JLabel("E", SwingConstants.CENTER);
+      arr[3] = new JLabel("R", SwingConstants.CENTER);
+      arr[4] = new JLabel("T", SwingConstants.CENTER);
+      arr[5] = new JLabel("Y", SwingConstants.CENTER);
+      arr[6] = new JLabel("U", SwingConstants.CENTER);
+      arr[7] = new JLabel("I", SwingConstants.CENTER);
+      arr[8] = new JLabel("O", SwingConstants.CENTER);
+      arr[9] = new JLabel("P", SwingConstants.CENTER);
+      arr[10] = new JLabel("A", SwingConstants.CENTER);
+      arr[11] = new JLabel("S", SwingConstants.CENTER);
+      arr[12] = new JLabel("D", SwingConstants.CENTER);
+      arr[13] = new JLabel("F", SwingConstants.CENTER);
+      arr[14] = new JLabel("G", SwingConstants.CENTER);
+      arr[15] = new JLabel("H", SwingConstants.CENTER);
+      arr[16] = new JLabel("J", SwingConstants.CENTER);
+      arr[17] = new JLabel("K", SwingConstants.CENTER);
+      arr[18] = new JLabel("L", SwingConstants.CENTER);
+      arr[19] = new JLabel("Z", SwingConstants.CENTER);
+      arr[20] = new JLabel("X", SwingConstants.CENTER);
+      arr[21] = new JLabel("C", SwingConstants.CENTER);
+      arr[22] = new JLabel("V", SwingConstants.CENTER);
+      arr[23] = new JLabel("B", SwingConstants.CENTER);
+      arr[24] = new JLabel("N", SwingConstants.CENTER);
+      arr[25] = new JLabel("M", SwingConstants.CENTER);
+   }  
+
+/**
+*Resets the colors of a Keyboard object
+*/    
    public void reset()
    {
-      gameboard.reset();
-      keyboard.reset();
-      scoreboard.update();
-      reset.setEnabled(false);
+      for(int k = 0; k < 26; k++) {
+         keyboard[k].setBackground(Color.WHITE);
+         keyboard[k].setForeground(Color.BLACK);
+      }
    }
-   
-   /**
-   *Sets the amount of guesses per game to the input x
-   @param x    assigns x to guessLimit
-   */
-   public void setGuessLimit(int x)
+ 
+/**
+*Updates the colors of individual letters on the QWERTY keyboard display depending on the player's guesses. 
+*/  
+   public static void updateKeyboard(String letter, int matchLevel)
    {
-      guessLimit = x;
-   }
-   
-   /**
-   *Returns the total amount of guesses in the current game
-   @return     guessLimit
-   */
-   public int getGuessLimit()
-   {
-      return guessLimit;
-   }
-   
-   /**
-   *Returns the reset JButton for use in other classes
-   @return     reset
-   */
-   public static JButton getResetButton()
-   {
-      return reset;
-   }
-   
-   //listener for the reset button
-   private class ResetListener implements ActionListener
-   {
-      public void actionPerformed(ActionEvent e)
-      {
-         //calls the WordlePanel's reset method
-         reset();
+      String alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM";
+      int i = alphabet.indexOf(letter);
+      if(matchLevel == 0) {
+         keyboard[i].setBackground(new Color(120, 124, 126));
+         keyboard[i].setForeground(Color.WHITE);
+      }
+      else if(matchLevel == 1) {
+         keyboard[i].setBackground(new Color(201, 180, 88));
+         keyboard[i].setForeground(Color.WHITE);
+      }
+      else {
+         keyboard[i].setBackground(new Color(106, 170, 100));
+         keyboard[i].setForeground(Color.WHITE);
       }
    }
 }
