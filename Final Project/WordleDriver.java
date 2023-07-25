05ffe3d46f2f5ab3e3c336982b87799a0a6f609f
nightshade1812 <126979626+nightshade1812@users.noreply.github.com>
2023-07-25 10:18:40 -0400

    edited panel size

--- Final Project/WordleDriver.java
+++ Final Project/WordleDriver.java
@@ -5,7 +5,7 @@ public class WordleDriver
    public static void main(String[] args)
    {
       JFrame frame = new JFrame("Wordle - Final Project (Sarah and Satik)");
-      frame.setSize(1000, 1000);
+      frame.setSize(400, 625);
       frame.setLocation(200, 100);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setContentPane(new WordlePanel());
