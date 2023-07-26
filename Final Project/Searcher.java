73a147f296a09cbc3d40184e0be58abf74e1ea3a
nightshade1812 <sgtrainer@cox.net>
2023-07-25 21:30:42 -0400

    Revert "stuff i guess"
    This reverts commit bff6b88320b9fe329b19ee4dce30a0f4df98ebff.

--- Final Project/Searcher.java
+++ Final Project/Searcher.java
@@ -9,15 +9,6 @@ public class Searcher
       return false;
    }
    
-   public static int linear(String[] array, String target, int index)
-   {
-      for(int i = 0; i < array.length; i++) {
-         if(target.equalsIgnoreCase(array[i]) && i != index)
-            return i;
-      }
-      return -1;
-   }
-   
    public static boolean binary(String[] array, String target, int first, int last)
    {
       int middle = (first + last)/2;
