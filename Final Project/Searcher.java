16a51b56404b36c65239981caedbda0e5a4d0dc5
Satik Karki <140443560+MusicalCheese@users.noreply.github.com>
2023-07-27 08:35:06 -0400

    Javadoc comments

--- Final Project/Searcher.java
+++ Final Project/Searcher.java
@@ -1,5 +1,16 @@
+//Created by Sarah Trainer, final version completed 7/24/2023
+
+/**
+*The Searcher class looks through <code>guesses.txt</code> and <code>answers.txt</code>, comparing it to the word that the user inputs.
+*/
 public class Searcher
 {
+   /**
+   *A linear search that compares the input word against <code>guesses.txt</code>.
+   @param   array - A String array, the list the search checks
+   @param   target - The input word being searched for
+   @return  boolean
+   */
    public static boolean linear(String[] array, String target)
    {
       for(int i = 0; i < array.length; i++) {
@@ -9,6 +20,14 @@ public class Searcher
       return false;
    }
    
+   /**
+   *A recursive binary search that compares the input word against <code>answers.txt</code>.
+   @param   array - A String array, the list the search checks
+   @param   target - The input word being searched for
+   @param   first - An <code>int</code> that determines where the search starts
+   @param   last - An <code>int</code> that determines where the search ends
+   @return  boolean
+   */
    public static boolean binary(String[] array, String target, int first, int last)
    {
       int middle = (first + last)/2;
