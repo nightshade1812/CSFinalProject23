fa78da5dd7a891b867735fb0a8bc997fd349a1a3
Satik Karki <140443560+MusicalCheese@users.noreply.github.com>
2023-07-27 08:33:31 -0400

    Javadoc comments

--- Final Project/DoubleLetterReader.java
+++ Final Project/DoubleLetterReader.java
@@ -1,5 +1,15 @@
+//Created by Sarah Trainer and Satik Karki, Final Version completed 7/27/2023
+
+/**
+*The DoubleLetterReader class checks for a special case involving the appearance of double letters in the answer versus in the guess
+*/
 public class DoubleLetterReader
 {
+  /**
+   *Reads and returns the characters that appear more than once in the guess as a String
+   @param   array - A <code>char</code> array 
+   @return  doubledChars
+   */ 
    public static String readDoubles(char[] array)
    {
       String doubledChars = "";
@@ -15,7 +25,12 @@ public class DoubleLetterReader
       }
       return doubledChars;
    }
-   
+   /**
+   *Returns the amount of times that a character appears in the guess
+   @param   array - a <code>char</code> array
+   @param   doubled - The character that appears more than once
+   @return  count
+   */
    public static int countOfChar(char[] array, char doubled)
    {
       int count = 0;
