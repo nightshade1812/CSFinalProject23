68d4376c17a39f349390c162c5fca14d06637ece
Satik Karki <140443560+MusicalCheese@users.noreply.github.com>
2023-07-27 08:38:18 -0400

    Javadoc comments

--- Final Project/WordleDriver.java
+++ Final Project/WordleDriver.java
@@ -1,11 +1,28 @@
+//Created by Sarah Trainer and Satik Karki, final version completed on 7/27/2023
+
 import javax.swing.*; //JPanel class
 import java.awt.event.*; //WindowListener class
 import java.awt.*; //Dimension class
 
+/**
+*WordleDriver is the driver containing the main executable code for Wordle. 
+*/
 public class WordleDriver
 {
+   /**
+   *A static instance of <code>Scoreboard</code>, used to keep track of game states and user statistics.
+   @see  Scoreboard
+   */
    static Scoreboard scoreboard;
-   
+
+   /**
+   *The main executable method, containing a <code>JFrame</code> which contains a <code>WordlePanel</code> instance.
+   *The main method also contains an instance of the <code>SaveAndClose</code> class, which implements <code>WindowAdaptor</code>.
+   *This class checks for when the <code>JFrame</code> is closed and then saves the game state and player statistics
+   @param   args - A array of strings created from written code. 
+   @see  JPanel
+   @see  WindowAdapter
+   */
    public static void main(String[] args)
    {
       scoreboard = new Scoreboard();
@@ -19,7 +36,13 @@ public class WordleDriver
       frame.setContentPane(new WordlePanel());
       frame.setVisible(true);
    }
-   
+
+   /**
+   *A private class that implements <code>WindowAdaptor</code>. A <code>SaveAndClose</code> instance checks to see if the window gets closed, then saves the 
+   *score by calling <code>Scoreboard</code>'s <code>saveData()</code> method. 
+   @see  WindowAdapter
+   @see  Scoreboard
+   */  
    private static class SaveAndClose extends WindowAdapter 
    {
       public void windowClosing(WindowEvent windowEvent) 
